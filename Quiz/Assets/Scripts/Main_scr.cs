4cd087976dc9b680297d34f8cf58d0c3fdb385db
lex-Under <fenr1r16n@yandex.ru>
2018-03-20 20:22:22 +0400

    v0.2
    -Бэкграунд
    -Стиль для кнопок

--- Quiz/Assets/Scripts/Main_scr.cs
+++ Quiz/Assets/Scripts/Main_scr.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,70 +8,66 @@ public class Main_scr : MonoBehaviour {
     
     // для контейнера тестов
     public GameObject ContainerTests;
-
     public Text TextQuestion;
-
     public Button[] ButtonAnswer;
-
     int CurrentQuestion;
-
-    string Tests = @"хуй или пизда1.дайте два.хуй.пизда
-хуй или пизда2.дайте два.хуй.пизда";
+    string Tests = @"Вас может соблазнить такая присущая ему (ей) черта, как.провокативность.сдержанность.загадочность.нежность.
+Для вас любовь втроем.это фантазия, которую можно воплотить в жизнь.шокирующая идея.заманчивый опыт (или уже испытанный).настоящий секс – дело двоих.
+О своей сексуальной жизни вы могли бы сказать, что она главным образом состоит из.подвигов.упоительных мгновений.обещаний.нежных ласк.
+Вашим секс-гаджетом мог бы стать (или является).набор игрушек, купленных в секс-шопе.презерватив.зеркало на потолке.шейный платок.
+Какова ваша первая ассоциация со словом «сексуальность».Два тела, слившихся воедино.Бутылка шампанского.Слова любви, выраженные языком тела.Райский сад, полный чудесных красок и ароматов.
+Каков ваш любимый момент во время близости.Момент оргазма.Я не выделяю какого-то конкретного момента.Предварительные ласки.Состояние нежной расслабленности после оргазма.
+Увидев на экране порноактера с рельефной мускулатурой, вы бы сказали.Вот это тело!.Какой ужас!.Они ничего не понимают в настоящей сексуальности.Это не для меня.
+Ваш лучший друг (подруга) признается вам, что хотел(а) бы иметь гомосексуальный опыт.Вас очень возбуждает это сообщение.Вам неловко обсуждать с ним (с ней) такую тему.Вы считаете, что ему (ей) нужна помощь психотерапевта.Вы разговариваете с ним (ней) об этом желании.
+Ваше сексуальное кредо.Камасутра.Ничего определенного.Главное – мои самые смелые фантазии.Желания любимого(ой) – закон.
+Сексуальный партнер в первую очередь должен быть.дерзким и пылким.сдержанным и вселяющим уверенность.страстным и изощренным.нежным и влюбленным.
+Помимо вашего партнера вы могли бы заняться сексом с.любовником(цей) старшего возраста.ни с кем.незнакомым человеком.бывшим возлюбленным(ой).
+Если бы секс воплотился в ткани, он стал бы.кожей красного цвета.белым хлопком.пурпурным бархатом.шелком пастельных тонов.
+Выразить партнеру свои пожелания в сексе – для вас это.азы сексуального общения.немыслимо.язык намеков.непросто, так как вы смущаетесь.
+Двое детей играют «в доктора» в стоге сена, cлыша их смех, вы думаете.Все-таки это нездоровые игры.А у них большое будущее!.Чудесная пора невинности!.Какая непосредственность!.
+Помните ли вы ту нежность, с которой ваши родители относились друг к другу.Нет, к ним это не относится.Такое было лишь по особым случаям.Такое бывало после каждой их ссоры.Да, с ностальгией вспоминаю об этом.
+Какая из частей тела способна вас взволновать больше всего.Половой орган.Кисти рук.Рот.Ягодицы.
+Какое из этих мест для вас наиболее эротично.Подъезд дома.Моя спальня.Пляж.Ванная.
+Бывают ли у вас эротические сны, от которых вы испытываете чувственное наслаждение.Часто.Никогда.С тех пор как миновал подростковый возраст – нет.Иногда.
+Мастурбация.полезна и расслабляет.это постыдное занятие.к ней прибегают за неимением партнера.это удовольствие, которое мы с партнером доставляем друг другу.";
     bool IsClick;
-
-    List<HZ> listHZ;
-
+    List<TestCase> listTest;
+    List<TestCase> listTestTmp;
 
     //Для контейнера меню
     public GameObject ContainerMenu;
-
     public Button ButtonStart;
 
 
     void Start ()
     {
-        InitializationTest();
+        //InitializationTest();
+        listTest = new List<TestCase>(TestCase.generateTestCases(Tests));
         ContainerTests.SetActive(false);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
     public void OnClickButtonStart()
     {
+        listTestTmp = new List<TestCase>(listTest);
         ContainerTests.SetActive(true);
         ContainerMenu.SetActive(false);
-        CurrentQuestion = UnityEngine.Random.Range(0, listHZ.Count - 1);
+        CurrentQuestion = UnityEngine.Random.Range(0, listTestTmp.Count - 1);
         ViewQuestion(CurrentQuestion);
         IsClick = true;
     }
-    void InitializationTest()
-    {
-        listHZ = new List<HZ>();
-        string[] str = Tests.Split('\n');
-        for (int i = 0; i < str.Length; i++)
-        {
-            listHZ.Add(new HZ(str[i]));
-        }  
-    }
     void ViewQuestion(int i)
     { 
-        TextQuestion.text = listHZ[i].Question;
-        int r = UnityEngine.Random.Range(0, listHZ[i].Answers.Count - 1);
-        ButtonAnswer[0].GetComponentInChildren<Text>().text = listHZ[i].Answers[r];
-        listHZ[i].Answers.RemoveAt(r);
-        r = UnityEngine.Random.Range(0, listHZ[i].Answers.Count - 1);
-        ButtonAnswer[1].GetComponentInChildren<Text>().text = listHZ[i].Answers[r];
-        listHZ[i].Answers.RemoveAt(r);
-        r = 0;
-        ButtonAnswer[2].GetComponentInChildren<Text>().text = listHZ[i].Answers[r];
+        TextQuestion.text = listTestTmp[i].Question;
+        string[] answersRandom = listTestTmp[i].getAnswersRandom();
+        for(int j=0; j<ButtonAnswer.Length; j++)
+        {
+           ButtonAnswer[j].GetComponentInChildren<Text>().text = answersRandom[j];
+        }
     }
     public void ButtonOnClick(int index)
     {
         if (IsClick)
         {
-            if (ButtonAnswer[index].GetComponentInChildren<Text>().text == listHZ[CurrentQuestion].CorrectAnswer)
+            if (ButtonAnswer[index].GetComponentInChildren<Text>().text == listTestTmp[CurrentQuestion].CorrectAnswer)
             {
                 ButtonAnswer[index].image.color = Color.green;
                 IsClick = false;
@@ -86,13 +83,13 @@ public class Main_scr : MonoBehaviour {
 
     public void NextQuestion()
     {
-        if (listHZ.Count > 1)
+        if (listTestTmp.Count > 1)
         {
-            listHZ.RemoveAt(CurrentQuestion);
+            listTestTmp.RemoveAt(CurrentQuestion);
             ButtonAnswer[2].image.color = Color.white;
             ButtonAnswer[1].image.color = Color.white;
             ButtonAnswer[0].image.color = Color.white;
-            CurrentQuestion = UnityEngine.Random.Range(0, listHZ.Count - 1);
+            CurrentQuestion = UnityEngine.Random.Range(0, listTestTmp.Count - 1);
             ViewQuestion(CurrentQuestion);
             IsClick = true;
         }
--- Quiz/Assets/Scripts/quiz.cs
+++ Quiz/Assets/Scripts/quiz.cs
@@ -1,17 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class HZ
+public class TestCase
 {
 	public string Question {get;set;}
 	public List<string>  Answers {get;set;}
 	public string CorrectAnswer {get;set;}
 
-	public HZ(string str)
+    /// <summary>
+    /// Метод генерирует массив вопросов с ответами на основе ключевой строки
+    /// </summary>
+    /// <param name="keyString"> ключевая строка </param>
+    /// <returns></returns>
+    public static TestCase[] generateTestCases(string keyString)
+    {
+        string[] stringTestCases = keyString.Split(new string[] { "\n", "\r" }, System.StringSplitOptions.RemoveEmptyEntries);
+        TestCase[] result = new TestCase[stringTestCases.Length];
+        for(int i=0; i<stringTestCases.Length;i++)
+        {
+            result[i] = new TestCase(stringTestCases[i]);
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Возвращает массив ответов на вопрос в случайном порядке
+    /// </summary>
+    /// <returns></returns>
+    public string[] getAnswersRandom()
+    {
+        List<string> answersList = new List<string>(Answers);
+        int count = answersList.Capacity;
+        //random replaces:
+        for (int i=0; i<count; i++)
+        {
+            int index1 = (int)UnityEngine.Random.Range(0, count);
+            int index2 = (int)UnityEngine.Random.Range(0, count);
+            string bucket = answersList[index1];
+            answersList[index1] = answersList[index2];
+            answersList[index2] = bucket;
+        }
+        return answersList.ToArray();
+    }
+    
+	public TestCase(string str)
 	{
         Answers = new List<string>();
-		string[] s=str.Split('.');
-		Question=s[0]+"?";
+		string[] s=str.Split(new char[] { '.' }, System.StringSplitOptions.RemoveEmptyEntries);
+		Question=s[0];
 		CorrectAnswer=s[1];
 		for(int i=0;i<s.Length - 1;i++)
 		{
